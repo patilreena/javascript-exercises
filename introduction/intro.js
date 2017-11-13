3d16679fe2bd6235939485318276079cdcb9c8a2
Reena Patil <reena.patil@outlook.com>
2017-11-13 22:06:01 +1100

    properties, built-in methods, libraries

--- introduction/intro.js
+++ introduction/intro.js
@@ -41,3 +41,32 @@ console.log(9 * 9);
 //Divide
 
 console.log( 100 / 10);
+
+//Properties
+
+console.log('javascript'.length);
+
+// Built-in Methods
+
+console.log('javascript'.toUpperCase());
+
+console.log('        javascript     '.trim());  // remove the whitespace at the beginning and end of the statement
+
+//libraries
+
+console.log(Math.random() * 50);
+
+console.log(Math.floor(Math.random() * 50));
+
+// Math.random generates a random number between 0 and 1.
+// then multiply that number by 50, so now it displays random number between 0 and 50.
+// Then, Math.floor rounds the number down to the nearest whole number.
+
+
+// Math.ceil() returns the smallest integer greater than or equal to a decimal number.
+
+console.log(Math.ceil(43.8));
+
+// Number.isInteger()checks if a number is an integer.
+
+console.log(Number.isInteger(99));
