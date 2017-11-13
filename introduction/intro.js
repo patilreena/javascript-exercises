b1f1774f5f15643bc6e7c498bbabcb64d3b266ea
Reena Patil <reena.patil@outlook.com>
2017-11-13 21:12:14 +1100

    math operators

--- introduction/intro.js
+++ introduction/intro.js
@@ -9,3 +9,35 @@ console.log('Hello');  // In this example, Hello! is logged to the console.
 // or double quotes is a string.
 
 console.log('JavaScript');
+
+
+// Numbers — Any number, including numbers with decimals:
+
+console.log(9);
+
+// Booleans — Either true or false, with no quotations
+
+console.log(true);
+
+// Null — Can only be null. It represents the absence of value.
+
+console.log(null);
+
+
+// Math Operators
+
+// Add
+
+console.log(10 + 20);
+
+//Subtract
+
+console.log(30 - 10);
+
+//Multiply
+
+console.log(9 * 9);
+
+//Divide
+
+console.log( 100 / 10);
