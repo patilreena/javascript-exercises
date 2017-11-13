f4f8277ef23186a2209ab0dcd163f985fc98e11f
Reena Patil <reena.patil@outlook.com>
2017-11-13 20:53:19 +1100

    data types

---
+++ introduction/intro.js
@@ -0,0 +1,11 @@
+// The console.log() command is used to print, or log, text to the console.
+
+console.log('Hello');  // In this example, Hello! is logged to the console. 
+
+
+// Data Types
+
+//strings - Any grouping of keyboard characters (letters, spaces, numbers, or symbols) surrounded by single quotes 
+// or double quotes is a string.
+
+console.log('JavaScript');
