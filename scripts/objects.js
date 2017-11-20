8c97072909975d5cbe63b081714e5f1e62235e58
Reena Patil <reena.patil@outlook.com>
2017-11-20 13:28:17 +1100

    date method

--- scripts/objects.js
+++ scripts/objects.js
@@ -10,3 +10,32 @@ const value = numbers.map((item) => {
   return item * 5;
 });
 console.log(value);
+
+let day = 25;
+let month = "december";
+let date = new Date(2017, 11, 25);
+
+
+console.log(`christmas is on ${date}`);
+
+
+let movies = {
+	name:"Forest Gump", 
+	year:1994
+}
+
+
+console.log(movies.name);
+
+
+let films = [{name:"Forest", year:2000}, {name:"Gump", year:1999}];
+
+// const movie = films((value) => {
+//   return value.name;
+// });
+
+console.log(films[0].name,films[0].year);
+
+
+
+
