34322c70fce642e5007b1351244c4ca5b02b4b23
Reena Patil <reena.patil@outlook.com>
2017-11-22 23:41:12 +1100

    Base class definitions

--- scripts/classes.js
+++ scripts/classes.js
@@ -21,3 +21,19 @@ const chicken_breast = new Food("chicken breast", 30, 0, 4);
 
 chicken_breast.print();
 
+// Creating Subclasses with extends & Calling with super
+
+class FatFreeFood extends Food {
+	constructor(name, protein, carbs){
+		super(name, protein, carbs, 0);
+	}
+
+	print(){
+		super.print();
+		console.log(`Would you look at that -- ${this.name} has no fat!`);
+	}
+}
+
+const fat_free_yogurt = new FatFreeFood('Greek Yogurt', 16, 12);
+fat_free_yogurt.print();
+
