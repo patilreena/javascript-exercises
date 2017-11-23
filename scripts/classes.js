faa0198499597be0178fb89c582d5ea5ad9e3b7b
Reena Patil <reena.patil@outlook.com>
2017-11-23 14:21:57 +1100

    a simple example of class

--- scripts/classes.js
+++ scripts/classes.js
@@ -37,3 +37,29 @@ class FatFreeFood extends Food {
 const fat_free_yogurt = new FatFreeFood('Greek Yogurt', 16, 12);
 fat_free_yogurt.print();
 
+
+// A simple example of class 
+
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }   
+
+  incrementBehavior() {
+    this._behavior ++;
+  }
+}
+const halley = new Dog('Halley');
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+halley.incrementBehavior(); // Add one to behavior
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
