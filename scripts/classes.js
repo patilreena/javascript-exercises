8dbeaa9724ea1ea054aeeb546c55228add89fc49
Reena Patil <reena.patil@outlook.com>
2017-11-24 14:06:30 +1100

    super keyword

--- scripts/classes.js
+++ scripts/classes.js
@@ -108,6 +108,36 @@ class HospitalEmployee {
   }
 }
 
+// super keyword
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name,certifications ) {
+    super(name);
+    this._certifications = certifications;
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk',['Trauma','Pediatrics']);
+
 
 
 
