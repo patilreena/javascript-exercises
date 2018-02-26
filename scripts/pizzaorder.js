1e92f708edb99916dc1c447ff594323456e2c489
Reena Patil <reena.patil@outlook.com>
2018-02-26 15:55:33 +1100

    calculating subtotal, total and tax

--- scripts/pizzaorder.js
+++ scripts/pizzaorder.js
@@ -1,5 +1,25 @@
+let orderCount = 0;
+
 function takeOrder(topping, crustType) {
   console.log('Order:' + crustType + 'crust pizza topped with' + topping);
+  orderCount = orderCount + 1;
+  // orderCount = orderCount * 10;
+}
+
+function getSubTotal(itemCount) {
+  return itemCount * 10;
+}
+
+function getTax() {
+  return getSubTotal(orderCount) * 20;
+}
+
+function getTotal() {
+  return getSubTotal(orderCount) + getTax();
 }
 
 takeOrder('chicken', 'thin');
+
+console.log(getSubTotal(orderCount));
+
+console.log(getTotal());
