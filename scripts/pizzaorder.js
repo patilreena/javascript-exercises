230ed4b9715a22c2b58df386222b0275bb5593d1
Reena Patil <reena.patil@outlook.com>
2018-02-26 15:48:57 +1100

    passing two parameters

--- scripts/pizzaorder.js
+++ scripts/pizzaorder.js
@@ -1,14 +1,5 @@
-let orderCount = 0;
-function takeOrder(topping, crustStyle) {
-  alert('Order:thin' + ' ' + crustStyle + ' ' + 'pizza' + ' ' + topping);
-  orderCount = orderCount + 1;
+function takeOrder(topping, crustType) {
+  console.log('Order:' + crustType + 'crust pizza topped with' + topping);
 }
 
-function subTotal(itemCount) {
-  return itemCount * 8;
-}
-
-takeOrder('chicken', 'egg');
-takeOrder('bacan', 'egg');
-
-alert(subTotal(orderCount));
+takeOrder('chicken', 'thin');
